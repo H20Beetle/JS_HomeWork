90921654d7813c0f467a304fc2764b27b99406e4
H20Beetle <waterbeetle091@gmail.com>
2023-09-14 02:15:39 -0500

    update

--- JS_Advanced_Concepts/main.js
+++ JS_Advanced_Concepts/main.js
@@ -53,4 +53,59 @@
 // console.log(user)
 
 // Facade Pattern
+// allows to change code in future while keeping older code clean
+// [[userId, 1]]
+// function getFetch(url, params = {}) {
+//     const queryString = Object.entries(params).map(param => {
+//         return `${param[0]=${param[1]}`
+//     }).join('&')
+//     return fetch(`${url}?${queryString}`, {
+//         method: 'GET',
+//         headers: { 'Content-Type': 'application/jason'}
+//     }).then(res => res.jason())
+// } 
+
+// function getFetch(url, params = {}) {
+//     return axios({
+//         url: url,
+//         method: 'GET',
+//         params: params
+//     }).then(res => res.data)
+// }
+
+
+//Observer Pattern
+// function createObseravable() {
+
+//     return{
+//         subscribers: [],
+
+//         subscribe(fn){
+//             this.subscribers.push(fn);
+//         },
+    
+//         unsubscribe(fn){
+//             this.subscribers = this.subscribers.filter((item) => item !== fn);
+//         },
+    
+//         broadcast(data){
+//             for(let i = 0; i< this.subscribers.length; i++){
+//                 this.subscribers[i](data);
+//             }
+//         },
+//     };
+// }  
+   
+    
+
+// const observer = createObseravable()
+
+// const fn = (data) =>{
+// console.log('Callback was executed with data', data)
+// };
+
+// observer.subscribe(fn);
+
+// observer.broadcast('Hello from the observable')
+
 
